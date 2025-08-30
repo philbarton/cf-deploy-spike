fc1b000c78cfba0648a29a556d03efe35b863f1a
philbarton <GRwWYPae9dCpzL>
2025-08-30 14:12:37 +0100

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
+    vite: {
+        ssr: {
+            // Treat Node built-ins as external for SSR
+            external: [
+                'node:crypto',
+                'node:fs',
+                'node:path',
+                'node:fs/promises',
+                'node:url',
+                'node:buffer'
+            ],
+        }
     },
-
-    imageService: "cloudflare"
-  })
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true
+        },
+        imageService: "cloudflare"
+    })
 });
